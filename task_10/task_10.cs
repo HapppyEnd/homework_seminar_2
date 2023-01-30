31449160cc09d6fe8c2bb9928ec0182f71570e19
Alena Savostyanova <el.savostyanova90@yandex.ru>
2023-01-30 23:12:52 +0300

    Homework.

---
+++ task_10/task_10.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+System.Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 99 || number > 999)
+{
+    System.Console.WriteLine("введите трехзначное число.");
+}
+else
+{
+    int result = number / 10;
+    System.Console.WriteLine(result % 10);
+}
---
+++ task_15/task_15.cs
@@ -0,0 +1,31 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите цифру от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string[] arr = {"Понедельник",
+                "Вторник",
+                "Среда",
+                "Четверг",
+                "Пятница",
+                "Суббота",
+                "Воскресенье"};
+
+if (number > 7 || number < 1)
+{ System.Console.WriteLine("Нет такого дня недели."); }
+else
+{
+    if (number == 6 || number == 7)
+    {
+        System.Console.WriteLine($"{arr[number - 1]} - Выходной!");
+    }
+    else
+    {
+        System.Console.WriteLine($"{arr[number - 1]} - Рабочий день(");
+    }
+}
